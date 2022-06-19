4e2abed283131715ccf5b37e9bfb0aceeb786790
Maximiliano Sosa <msosa89@outlook.com>
2022-06-19 05:24:33 -0300

    projectsPage styles (#4)

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -10,10 +10,10 @@ export default function ProjectsCard ({project}) {
                 <h2 className="text-3xl uppercase m-1 text-white font-bold text-center">{project.projectTitle}</h2>
                 <p className="text-base m-1 my-5 text-white text-justify">{project.projectSummary}</p>
                 <div className="text-center">
-                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Detalles</button>
-                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Sitio</button>
-                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Repo Front</button>
-                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Repo Back</button>
+                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 lg:px-2 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Detalles</button>
+                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 lg:px-2 m-1 rounded-full w-full md:w-2/5 lg:w-1/6">Sitio</button>
+                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 lg:px-2 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Repo Front</button>
+                    <button className="border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold py-2 px-4 lg:px-2 m-1 rounded-full w-full md:w-2/5 lg:w-1/5">Repo Back</button>
                 </div>
             </div>
         </div>
