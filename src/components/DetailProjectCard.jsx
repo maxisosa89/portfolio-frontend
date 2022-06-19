536bc68ed7094cd3f7d2dfe22d89d44a5a68b34e
unknown <msosa89@outlook.com>
2022-06-19 08:14:22 -0300

    render detail project

--- src/components/DetailProjectCard.jsx
+++ src/components/DetailProjectCard.jsx
@@ -3,7 +3,23 @@ import React from "react";
 export default function DetailProjectCard ({project}) {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 border-solid border border-violet-600 bg-violet-500">
-            <h1>Detail Project Card</h1>
+            <h1>{project.projectTitle}</h1>
+            {
+                project.projectImg.map(img => (
+                    <img src={img} alt="Not found" key={img}/>
+                ))
+            }
+            <h3>Descripción</h3>
+            <p>{project.projectDescription}</p>
+            <h3>Tecnologías</h3>
+            {
+                project.tech.map(t => (
+                    <div key={t.techTitle}>
+                        <img src={t.techImg} alt="Not found" />
+                        <p>{t.techTitle}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- src/pages/DetailProjectPage.jsx
+++ src/pages/DetailProjectPage.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import DetailProjectCard from "../components/DetailProjectCard";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 export default function DetailProjectPage () {
     const [project, setProject] = useState();
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
     const getProject = async () => {
-        const proj = await axios.get('http://localhost:3001/projects');
+        const proj = await axios.get(`http://localhost:3001/projects/${id}`);
         console.log(proj.data);
         setProject(proj.data);
+        setLoading(false);
     };
     useEffect(() => {
         getProject();
@@ -15,7 +20,11 @@ export default function DetailProjectPage () {
     return (
         <div className="container mx-auto bg-violet-900 pt-24">
             <h1 className="text-5xl text-center text-white font-bold uppercase">Detail Project</h1>
-            <DetailProjectCard />
+            {
+                loading ?
+                <Spinner /> :
+                <DetailProjectCard project={project} />
+            }
         </div>
     )
 }
