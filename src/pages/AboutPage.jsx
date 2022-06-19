2ad5f714cec217cd22ef4f636990aa507602a6d1
unknown <msosa89@outlook.com>
2022-06-19 06:11:39 -0300

    create pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutPage () {
+    return (
+        <div className="container mx-auto bg-violet-900 pt-24">
+            <h1 className="text-5xl text-center text-white font-bold uppercase">AboutPage</h1>
+        </div>
+    )
+}
---
+++ src/pages/SkillsPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SkillsPage () {
+    return (
+        <div className="container mx-auto bg-violet-900 pt-24">
+            <h1 className="text-5xl text-center text-white font-bold uppercase">SkillsPage</h1>
+        </div>
+    )
+}
