432a2c8017cf682bb3e2e701116ad49f3463a4a0
Maximiliano Sosa <msosa89@outlook.com>
2022-06-20 00:12:07 -0300

    fix navbar (#12)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function NavBar () {
+    const location = useLocation().pathname;
     function handleToggle() {
-        document.getElementById("containerData").classList.toggle("hidden");
+        window.innerWidth < 1024 && document.getElementById("containerData").classList.toggle("hidden");
     }
     return (
         <nav className="flex items-center justify-between flex-wrap z-50 bg-violet-700 p-6 fixed w-full">
@@ -11,34 +12,34 @@ export default function NavBar () {
                 <span className="font-semibold text-xl tracking-tight">LOGO</span>
             </div>
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-300 hover:border-gray-300" onClick={handleToggle}>
+                <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-400 hover:border-gray-300" onClick={handleToggle}>
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
             <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden" id="containerData">
                 <div className="text-sm lg:flex-grow">
-                    <NavLink to='/about' onClick={handleToggle}>
-                        <p className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+                    <NavLink to='/about' onClick={handleToggle} value="about">
+                        <p className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 ${location.includes("about") && 'text-gray-400'}`}>
                             Sobre mí
                         </p>
                     </NavLink>
-                    <NavLink to='/projects' onClick={handleToggle}>
-                        <p className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+                    <NavLink to='/projects' onClick={handleToggle} value="projects">
+                        <p className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 ${location.includes("projects") && 'text-gray-400'}`}>
                             Mis Proyectos
                         </p>
                     </NavLink>
-                    <NavLink to='/skills' onClick={handleToggle}>
-                        <p className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+                    <NavLink to='/skills' onClick={handleToggle} value="skils">
+                        <p className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 ${location.includes("skills") && 'text-gray-400'}`}>
                             Skills
                         </p>
                     </NavLink>
-                    <NavLink to='/contact' onClick={handleToggle}>
-                        <p className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4">
+                    <NavLink to='/contact' onClick={handleToggle} value="contact">
+                        <p className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 ${location.includes("contact") && 'text-gray-400'}`}>
                             Contacto
                         </p>
                     </NavLink>
-                    <NavLink to='/admin/dashboard' onClick={handleToggle}>
-                        <p className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300">
+                    <NavLink to='/admin/dashboard' onClick={handleToggle} value="adminDashboard">
+                        <p className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 ${location.includes("admin") && 'text-gray-400'}`}>
                             Admin
                         </p>
                     </NavLink>
