c4c1eb87adae06792998e1572b27ca529e2399a9
unknown <msosa89@outlook.com>
2022-06-19 23:04:51 -0300

    create carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+export default function Carousel ({project}) {
+    const [count, setCount] = useState(0);
+    const [currentImg, setCurrentImg] = useState(project.projectImg[count]);
+    function handleSlide(e) {
+        e.preventDefault();
+        if (e.target.value === "prev"){
+            count > 0 ? 
+            setCount(count-1) :
+            setCount(project.projectImg.length-1)
+        } else if (e.target.value === "next") {
+            count < project.projectImg.length-1 ? 
+            setCount(count+1) :
+            setCount(0)
+        } else if (parseInt(e.target.value) >= 0 && parseInt(e.target.value) < project.projectImg.length) {
+            setCount(parseInt(e.target.value));
+        }
+    };
+    useEffect(() => {
+        setCurrentImg(project.projectImg[count]);
+    }, [count]);
+    return (
+        <div className="relative grid h-96 w-full">
+            <img className="h-96 w-full object-cover" src={currentImg} alt="not found" />
+            <div className="flex justify-between self-end absolute w-full">
+                <button
+                    className="w-10 h-10 m-2 border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold text-2xl flex align-middle justify-center rounded-full"
+                    value="prev"
+                    onClick={handleSlide}>
+                    {'<'}
+                </button>
+                <div className="flex justify-center self-center">
+                    {
+                        project.projectImg.map((e, index) => (
+                            <button
+                                className={`w-4 h-4 m-2 border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold text-2xl flex align-middle justify-center rounded-full ${index === count && 'bg-violet-900 hover:bg-violet-900'}`}
+                                id={index}
+                                value={index}
+                                key={index}
+                                onClick={handleSlide}>
+                            </button>
+                        ))
+                    }
+                </div>
+                <button
+                    className="w-10 h-10 m-2 border border-white bg-violet-500 hover:bg-violet-600 text-white font-semibold text-2xl flex align-middle justify-center rounded-full"
+                    value="next"
+                    onClick={handleSlide}>
+                    {'>'}
+                </button>
+            </div>
+        </div>
+    )
+}
