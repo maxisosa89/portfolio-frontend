cecdafb60da37670875c150c0f101dd0f542ab3a
unknown <msosa89@outlook.com>
2022-06-19 23:09:16 -0300

    fix menor bug

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Carousel from "../components/Carousel"
+import Carousel from "./Carousel"
 
 export default function ProjectsCard ({project}) {
     return (
