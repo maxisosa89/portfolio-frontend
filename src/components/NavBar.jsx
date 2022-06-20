ae4d3468ff008e02854da87d101a9a9e1c1acc99
Maximiliano Sosa <msosa89@outlook.com>
2022-06-20 08:32:50 -0300

    fix navbar (#16)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,19 @@ export default function NavBar () {
     function handleToggle() {
         window.innerWidth < 1024 && document.getElementById("containerData").classList.toggle("hidden");
     }
+    document.addEventListener('click', function(event) {
+        if(event.target.id !== 'containerData' && event.target.id !== "btn" && event.target.id !== "svg" && window.innerWidth < 1024){
+          document.getElementById('containerData').classList.add("hidden");
+        }
+    });
     return (
         <nav className="flex items-center justify-between flex-wrap z-50 bg-primary p-6 fixed w-full">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <span className="font-semibold text-xl tracking-tight">LOGO</span>
             </div>
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-tertiary hover:border-tertiary" onClick={handleToggle}>
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <button id="btn" className="px-3 py-2 border rounded text-white border-white hover:text-tertiary hover:border-tertiary" onClick={handleToggle}>
+                    ☰
                 </button>
             </div>
             <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden" id="containerData">
