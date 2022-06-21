1c906bd34a01300dcd0c18677798b0cf08cbee50
unknown <msosa89@outlook.com>
2022-06-21 07:39:05 -0300

    add modal

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -28,17 +28,24 @@ export default function ContactPage () {
         setLoading(true);
         await axios.post("http://localhost:3001/messages", dataForm);
         handleClear(e);
+        document.getElementById("modalSucces").classList.remove("hidden");
+    };
+    function handleCloseModal(e) {
+        e.preventDefault();
+        document.getElementById("modalSucces").classList.add("hidden");
         setLoading(false);
     };
     useEffect(() => {
         console.log(dataForm);
     }, [dataForm])
     return (
-        <div className="container mx-auto bg-tertiary pt-24">
+        <div className="container relative mx-auto bg-tertiary pt-24">
             <h1 className="text-5xl text-center text-white font-bold uppercase">Contacto</h1>
             {
                 loading ?
-                <Spinner /> :
+                <div className="fixed top-1/2 left-1/2 p-5 transform -translate-x-1/2 -translate-y-1/2">
+                    <Spinner />
+                </div> :
                 <form className="flex flex-wrap w-full max-w-[1024px] bg-secondary mx-auto p-10">
                     <div className="w-full lg:w-1/2 px-3 py-3">
                         <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="name">
@@ -92,6 +99,28 @@ export default function ContactPage () {
                     </div>
                 </form>
             }
+            {/* Modal */}
+            <div id="modalSucces" className="hidden fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full sm:max-w-[400px] bg-secondary text-white border border-white rounded py-3 px-4">
+                <div className="">
+                    <h1>Mensaje enviado</h1>
+                </div>
+                <hr className="border border-primary" />
+                <div className="py-5">
+                    <p>
+                        Gracias por tu contacto, estaré contestando a la brevedad.
+                        <br/>
+                        <br/>
+                        Saludos.
+                    </p>
+                </div>
+                <div className="flex justify-center">
+                    <button
+                        className="bg-secondary hover:bg-tertiary text-white border border-white rounded py-1 px-4 h-10"
+                        onClick={handleCloseModal}>
+                            Continuar
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
