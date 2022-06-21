1ca44a1a681979988cb700a5961412eb3c9d678b
Maximiliano Sosa <msosa89@outlook.com>
2022-06-21 04:17:21 -0300

    card 0035 (#18)

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ContactPage () {
+    const [dataForm, setDataForm] = useState({
+        name: '',
+        mail: '',
+        message: ''
+    });
+    function handleChange(e) {
+        setDataForm({
+            ...dataForm,
+            [e.target.id]: e.target.value
+        });
+    };
+    function handleClear(e) {
+        e.preventDefault();
+        setDataForm({
+            name: '',
+            mail: '',
+            message: ''
+        });
+    };
+    function handleSubmit(e) {
+        e.preventDefault();
+    };
+    useEffect(() => {
+        console.log(dataForm);
+    }, [dataForm])
     return (
         <div className="container mx-auto bg-tertiary pt-24">
             <h1 className="text-5xl text-center text-white font-bold uppercase">Contacto</h1>
@@ -9,25 +34,51 @@ export default function ContactPage () {
                     <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="name">
                         Nombre
                     </label>
-                    <input className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight" id="name" type="text" placeholder="Nombre..." />
+                    <input
+                        value={dataForm.name}
+                        onChange={handleChange}
+                        className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                        id="name"
+                        type="text"
+                        placeholder="Nombre..." />
                 </div>
                 <div className="w-full lg:w-1/2 px-3 py-3">
-                    <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="email">
+                    <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="mail">
                         Email
                     </label>
-                    <input className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight" id="email" type="text" placeholder="Email..." />
+                    <input
+                        value={dataForm.mail}
+                        onChange={handleChange}
+                        className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                        id="mail"
+                        type="text"
+                        placeholder="Email..." />
                 </div>
                 <div className="w-full px-3 py-3">
                     <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="message">
                         Mensaje
                     </label>
-                    <textarea className="appearance-none block w-full h-[200px] bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight" id="message" type="text" placeholder="Mensaje..." />
+                    <textarea
+                        value={dataForm.message}
+                        onChange={handleChange}
+                        className="appearance-none block w-full h-[200px] bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                        id="message"
+                        type="text"
+                        placeholder="Mensaje..." />
                 </div>
                 <div className="w-full lg:w-1/2 px-3 py-3">
-                    <button className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">Enviar</button>
+                    <button
+                        onClick={handleSubmit}
+                        className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
+                            Enviar
+                    </button>
                 </div>
                 <div className="w-full lg:w-1/2 px-3 py-3">
-                    <button className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">Limpiar</button>
+                    <button
+                        onClick={handleClear}
+                        className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
+                            Limpiar
+                    </button>
                 </div>
             </form>
         </div>
