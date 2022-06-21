866c041c9610ad9cf03f9a85c9af60d42682b7df
Maximiliano Sosa <msosa89@outlook.com>
2022-06-21 05:33:20 -0300

    create-new-message-on-contact-form (#19)

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
 
 export default function ContactPage () {
+    const [loading, setLoading] = useState(false);
     const [dataForm, setDataForm] = useState({
         name: '',
         mail: '',
@@ -20,8 +23,12 @@ export default function ContactPage () {
             message: ''
         });
     };
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
+        setLoading(true);
+        await axios.post("http://localhost:3001/messages", dataForm);
+        handleClear(e);
+        setLoading(false);
     };
     useEffect(() => {
         console.log(dataForm);
@@ -29,58 +36,62 @@ export default function ContactPage () {
     return (
         <div className="container mx-auto bg-tertiary pt-24">
             <h1 className="text-5xl text-center text-white font-bold uppercase">Contacto</h1>
-            <form className="flex flex-wrap w-full max-w-[1024px] bg-secondary mx-auto p-10">
-                <div className="w-full lg:w-1/2 px-3 py-3">
-                    <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="name">
-                        Nombre
-                    </label>
-                    <input
-                        value={dataForm.name}
-                        onChange={handleChange}
-                        className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
-                        id="name"
-                        type="text"
-                        placeholder="Nombre..." />
-                </div>
-                <div className="w-full lg:w-1/2 px-3 py-3">
-                    <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="mail">
-                        Email
-                    </label>
-                    <input
-                        value={dataForm.mail}
-                        onChange={handleChange}
-                        className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
-                        id="mail"
-                        type="text"
-                        placeholder="Email..." />
-                </div>
-                <div className="w-full px-3 py-3">
-                    <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="message">
-                        Mensaje
-                    </label>
-                    <textarea
-                        value={dataForm.message}
-                        onChange={handleChange}
-                        className="appearance-none block w-full h-[200px] bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
-                        id="message"
-                        type="text"
-                        placeholder="Mensaje..." />
-                </div>
-                <div className="w-full lg:w-1/2 px-3 py-3">
-                    <button
-                        onClick={handleSubmit}
-                        className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
-                            Enviar
-                    </button>
-                </div>
-                <div className="w-full lg:w-1/2 px-3 py-3">
-                    <button
-                        onClick={handleClear}
-                        className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
-                            Limpiar
-                    </button>
-                </div>
-            </form>
+            {
+                loading ?
+                <Spinner /> :
+                <form className="flex flex-wrap w-full max-w-[1024px] bg-secondary mx-auto p-10">
+                    <div className="w-full lg:w-1/2 px-3 py-3">
+                        <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="name">
+                            Nombre
+                        </label>
+                        <input
+                            value={dataForm.name}
+                            onChange={handleChange}
+                            className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                            id="name"
+                            type="text"
+                            placeholder="Nombre..." />
+                    </div>
+                    <div className="w-full lg:w-1/2 px-3 py-3">
+                        <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="mail">
+                            Email
+                        </label>
+                        <input
+                            value={dataForm.mail}
+                            onChange={handleChange}
+                            className="appearance-none block w-full bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                            id="mail"
+                            type="text"
+                            placeholder="Email..." />
+                    </div>
+                    <div className="w-full px-3 py-3">
+                        <label className="text-xl uppercase tracking-wide text-white font-bold mb-2" htmlFor="message">
+                            Mensaje
+                        </label>
+                        <textarea
+                            value={dataForm.message}
+                            onChange={handleChange}
+                            className="appearance-none block w-full h-[200px] bg-tertiary text-white border border-white rounded py-3 px-4 leading-tight"
+                            id="message"
+                            type="text"
+                            placeholder="Mensaje..." />
+                    </div>
+                    <div className="w-full lg:w-1/2 px-3 py-3">
+                        <button
+                            onClick={handleSubmit}
+                            className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
+                                Enviar
+                        </button>
+                    </div>
+                    <div className="w-full lg:w-1/2 px-3 py-3">
+                        <button
+                            onClick={handleClear}
+                            className="w-full bg-secondary hover:bg-tertiary text-white border font-semibold border-white rounded py-3 px-4 leading-tight">
+                                Limpiar
+                        </button>
+                    </div>
+                </form>
+            }
         </div>
     )
 }
