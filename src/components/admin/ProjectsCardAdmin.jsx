cbb5162ee0d2485c0402d64f6dd9565a8cadfc60
unknown <msosa89@outlook.com>
2022-06-24 07:31:57 -0300

    0054-mobile-version-projects-cards-admin

--- src/components/admin/ProjectsCardAdmin.jsx
+++ src/components/admin/ProjectsCardAdmin.jsx
@@ -3,26 +3,28 @@ export default function ProjectsCardAdmin({ project }) {
     return (
         <div>
             <hr className="my-1 mx-5 border border-gray-400" />
-            <div className="flex mx-8">
-                <div className="w-3/5 h-32 lg:h-64 mx-2 mt-0.5 text-xl md:text-2xl lg:text-4xl flex">
-                    <p className="w-full text-center self-center break-words">{project.projectTitle}</p>
+            <div className="flex flex-wrap my-2 mx-7 md:h-64">
+                <img
+                    src={project.projectImg[0]}
+                    alt="Imagen no disponible"
+                    className="w-full md:w-5/12 h-48 md:h-64 object-cover"
+                />
+                <div className="w-full md:w-6/12 text-4xl p-5 text-center self-center">
+                    <p className="break-all">{project.projectTitle}</p>
                 </div>
-                <div className="w-2/5 h-32 lg:h-64 py-2 px-0 flex ">
-                    <img
-                        src={project.projectImg[0]}
-                        alt="Imagen no disponible"
-                        className="w-10/12 h-full object-cover"
-                    />
-                    <div className="w-2/12 mt-4 lg:mt-8">
+                <div className="w-full md:w-1/12 self-center">
+                    <div className="flex md:inline justify-center">
                         <img
                             src="https://res.cloudinary.com/dg7fmdsmw/image/upload/v1656060522/Portfolio/iconos/edit_hjowes.png"
                             alt="Editar"
-                            className="relative inset-x-2 inset-y-px w-7 h-7 md:w-11 md:h-11"
+                            className="h-16 mx-2 md:my-2 border-4 border-blue-900 rounded cursor-pointer hover:bg-blue-500"
+                            onClick={e => console.log("editar")}
                         />
                         <img
                             src="https://res.cloudinary.com/dg7fmdsmw/image/upload/v1656060758/Portfolio/iconos/delete_g3pwe2.png"
                             alt="Eliminar"
-                            className="relative inset-x-2 inset-y-px w-7 h-7 md:w-11 md:h-11"
+                            className="h-16 mx-2 md:my-2 border-4 border-red-900 rounded cursor-pointer hover:bg-red-500"
+                            onClick={e => console.log("borrar")}
                         />
                     </div>
                 </div>
