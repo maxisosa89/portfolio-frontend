87edf127301f7ac12ca85f06d2e8725299ecdd2a
unknown <msosa89@outlook.com>
2022-07-03 12:17:42 -0300

    dashboard page

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function DashboardPage () {
     return (
-        <div className="container mx-auto bg-tertiary pt-24">
-            <h1 className="text-5xl text-center text-white font-bold uppercase">DashboardPage</h1>
+        <div className="flex flex-wrap justify-center container mx-auto bg-tertiary pt-24">
+            <div className="w-full sm:w-40">
+                <p className="text-white font-semibold text-2xl text-center">Mensajes</p>
+                <div className="flex justify-center">
+                    <NavLink to="/admin/messages">
+                        <button
+                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                        >
+                            {'5/20'}
+                        </button>
+                    </NavLink>
+                </div>
+            </div>
+            <div className="w-full sm:w-40">
+                <p className="text-white font-semibold text-2xl text-center">Proyectos</p>
+                <div className="flex justify-center">
+                    <NavLink to="/admin/projects">
+                        <button
+                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                        >
+                            {'10'}
+                        </button>
+                    </NavLink>
+                </div>
+            </div>
+            <div className="w-full sm:w-40">
+                <p className="text-white font-semibold text-2xl text-center">Techs</p>
+                <div className="flex justify-center">
+                    <NavLink to="/admin/techs">
+                        <button
+                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                        >
+                            {'10'}
+                        </button>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 }
