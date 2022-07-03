19317598d60f074bb06557a427cdf8c15221ff33
unknown <msosa89@outlook.com>
2022-07-03 12:43:07 -0300

    dashboard logic

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -1,45 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import Spinner from "../../components/Spinner";
 
 export default function DashboardPage () {
+    const [messages, setMessages] = useState();
+    const [unreadMessages, setUnreadMessages] = useState();
+    const [projects, setProjects] = useState();
+    const [techs, setTechs] = useState();
+    const [loading, setLoading] = useState(true);
+    async function getData(){
+        const m = await axios.get('http://localhost:3001/messages');
+        const p = await axios.get('http://localhost:3001/projects');
+        const t = await axios.get('http://localhost:3001/techs');
+        setMessages(m.data.length);
+        setUnreadMessages(m.data.filter(m => !m.read).length);
+        setProjects(p.data.length);
+        setTechs(t.data.length);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getData();
+    }, []);
     return (
-        <div className="flex flex-wrap justify-center container mx-auto bg-tertiary pt-24">
-            <div className="w-full sm:w-40">
-                <p className="text-white font-semibold text-2xl text-center">Mensajes</p>
-                <div className="flex justify-center">
-                    <NavLink to="/admin/messages">
-                        <button
-                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
-                        >
-                            {'5/20'}
-                        </button>
-                    </NavLink>
+        <div className="container mx-auto bg-tertiary pt-24">
+            {
+                loading ?
+                <div className="fixed top-1/2 left-1/2 p-5 transform -translate-x-1/2 -translate-y-1/2">
+                    <Spinner />
+                </div> :
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-full sm:w-40">
+                        <p className="text-white font-semibold text-2xl text-center">Mensajes</p>
+                        <div className="flex justify-center">
+                            <NavLink to="/admin/messages">
+                                <button
+                                    className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                                >
+                                    { `${unreadMessages}/${messages}`}
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="w-full sm:w-40">
+                        <p className="text-white font-semibold text-2xl text-center">Proyectos</p>
+                        <div className="flex justify-center">
+                            <NavLink to="/admin/projects">
+                                <button
+                                    className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                                >
+                                    {projects}
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="w-full sm:w-40">
+                        <p className="text-white font-semibold text-2xl text-center">Techs</p>
+                        <div className="flex justify-center">
+                            <NavLink to="/admin/techs">
+                                <button
+                                    className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
+                                >
+                                    {techs}
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="w-full sm:w-40">
-                <p className="text-white font-semibold text-2xl text-center">Proyectos</p>
-                <div className="flex justify-center">
-                    <NavLink to="/admin/projects">
-                        <button
-                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
-                        >
-                            {'10'}
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
-            <div className="w-full sm:w-40">
-                <p className="text-white font-semibold text-2xl text-center">Techs</p>
-                <div className="flex justify-center">
-                    <NavLink to="/admin/techs">
-                        <button
-                            className="w-28 h-28 m-2 border border-white bg-secondary hover:bg-tertiary text-white font-semibold text-2xl rounded-full"
-                        >
-                            {'10'}
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
+            }
         </div>
     )
 }
