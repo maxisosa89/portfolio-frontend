8773cc9ea159deab94bbfa8237ec88ca2ebb3eb2
Maximiliano Sosa <msosa89@outlook.com>
2022-08-04 15:00:48 -0300

    404 notfound page (#53)

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NotFoundPage () {
+  return (
+    <div className="pt-48">
+      <div className="h-full flex justify-center items-center">
+        <h3 className="text-white text-3xl text-center">La página que estas buscando no existe</h3>
+      </div>
+    </div>
+  );
+};
