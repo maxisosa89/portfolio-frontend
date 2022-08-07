060c53d844cc87cf5e37554de01183e855c03c6b
Maximiliano Sosa <msosa89@outlook.com>
2022-08-07 10:46:19 -0300

    user logged verification (#55)

--- src/middlewares/auth.jsx
+++ src/middlewares/auth.jsx
@@ -1,31 +1,33 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import Spinner from "../components/Spinner";
+import jwt_decode from "jwt-decode";
 
-export default function PrivateRoute ({Component}) {
-    const [auth, setAuth] = useState(false);
-    const [loading, setLoading] = useState(true);
-    async function getToken() {
-        try {
-            const token = localStorage.getItem("tokenPortfolioMS");
-            const validateToken = await axios.get('/messages', { headers: { Authorization: token } });
-            if (validateToken.status === 200) {
-                setAuth(true);
-                setLoading(false);
-            };
-        } catch (e) {
-            console.log(e);
-            setAuth(false);
-            setLoading(false);
-        };
-    };
-    useEffect(() => {
-        getToken()
-    }, []);
-    return loading ?
-        <div className="fixed top-1/2 left-1/2 p-5 transform -translate-x-1/2 -translate-y-1/2">
-            <Spinner />
-        </div> :
-        auth === true ? <Component /> : <Navigate to="/login" />
+export default function PrivateRoute({ Component }) {
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+  function getToken() {
+    try {
+      const token = localStorage.getItem("tokenPortfolioMS");
+      const decoded = jwt_decode(token);
+      const current_time = new Date().getTime() / 1000;
+      if (current_time > decoded.exp) {
+        setAuth(false);
+      } else {
+        setAuth(true);
+      }
+      setLoading(false);
+    } catch (e) {
+      setAuth(false);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getToken()
+  }, []);
+  return loading ?
+    <div className="fixed top-1/2 left-1/2 p-5 transform -translate-x-1/2 -translate-y-1/2">
+      <Spinner />
+    </div> :
+    auth === true ? <Component /> : <Navigate to="/login" />
 }
